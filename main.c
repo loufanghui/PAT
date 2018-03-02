b0dc081447ca0d629bf65c1e55da38be90af9a99
loufanghui <35704441+loufanghui@users.noreply.github.com>
2018-03-02 15:56:27 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "G:\untitled3\practical project\temperature.h"
+
+int main( void )
+{
+    float h;
+    h =calculate();
+    printf( "%f", h );
+    system("pause");
+    return 0 ;
+}
